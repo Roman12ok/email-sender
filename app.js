639f86aeb749576bd7abb06b799c98cbd23515bf
Roman English Ahmadu <ahmaduromanenglish@gmail.com>
2023-07-17 18:53:52 +0200

    code update

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3004
+const express = require('express');
+const app = express();
+const port = 3001;
+const cors = require('cors');
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome')
+})
+
+app.post('/api', (req, res) => {
+  const a = { Email, Name, Numd, Message } = req.body;
+  console.table(a);
+  res.send('Received the data successfully'); 
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
