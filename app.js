d9eb57f5e520e926f66356c5018328f239a25feb
Roman English Ahmadu <ahmaduromanenglish@gmail.com>
2023-07-17 19:03:04 +0200

    use .env let see

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+require('dotenv').config()
+const port = process.env.PORT;
 const cors = require('cors');
 
 app.use(cors());
