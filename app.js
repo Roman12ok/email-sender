789f0f64e13ede915ad1ed694ce494d6e2d8d929
Roman English Ahmadu <ahmaduromanenglish@gmail.com>
2023-07-19 21:17:31 +0200

    ewf

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
-const port = 5000;
+const port = 500;
 const cors = require('cors');
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
 app.use(cors());
 app.use(express.json());
@@ -14,29 +14,32 @@ app.post('/api', (req, res) => {
   res.send('Received the data successfully'); 
 });
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'romanenglish889@gmail.com',
-    pass: 'yvnhumrlymjmutoa' // naturally, replace both with your real credentials or an application-specific password
-  }
-});
+app.get('/r', (req, res) => {
+  res.send('dfgdgegre')
+})
 
-const mailOptions = {
-  from: 'romanenglish889@gmail.com',
-  to: 'romanenglish889@gmail.com',
-  subject: 'Invoices due',
-  text: 'Dsvewney.',
-  html: ``,
-};
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'romanenglish889@gmail.com',
+//     pass: 'yvnhumrlymjmutoa' // naturally, replace both with your real credentials or an application-specific password
+//   }
+// });
 
-transporter.sendMail(mailOptions, (error, info) =>{
-  if (error) {
-	console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+// const mailOptions = {
+//   from: 'romanenglish889@gmail.com',
+//   to: 'romanenglish889@gmail.com',
+//   subject: 'Invoices due',
+//   text: 'Dsvewney.',
+// };
+
+// transporter.sendMail(mailOptions, (error, info) =>{
+//   if (error) {
+// 	console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
 
 
-app.listen(process.env.PORT || port, () => console.log(`listening on port ${port}!`));
+app.listen(port, () => console.log(`listening on port ${port}!`));
