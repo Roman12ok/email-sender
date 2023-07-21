add83e5ef56d1277a6e547236306e6f2203ecb9a
Roman English Ahmadu <ahmaduromanenglish@gmail.com>
2023-07-21 20:07:23 +0200

    wewr

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_ADDRESS,
-    pass: 'yvnhumrlymjmutoa' 
+    pass: process.env.PASSWORDS 
   }
 });
 
