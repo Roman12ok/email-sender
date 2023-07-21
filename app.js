7be643074d07acc55a254820f2dbcc084392d2f9
Roman English Ahmadu <ahmaduromanenglish@gmail.com>
2023-07-21 19:43:28 +0200

    message

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
-const port = 500;
+require('dotenv').config();
+const port = 3013;
 const cors = require('cors');
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/api', (req, res) => {
-  var a = { Email, Name, Numd, Message } = req.body;
-  console.table(a);
-  res.send('Received the data successfully'); 
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: 'yvnhumrlymjmutoa' 
+  }
 });
 
-app.get('/r', (req, res) => {
-  res.send('dfgdge34gre')
-})
+app.post('/api', (req, res) => {
+  const { Email, Name, Numd, Message } = req.body;
+  console.table(req.body); 
+  res.send('Received the data successfully');
 
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'romanenglish889@gmail.com',
-//     pass: 'yvnhumrlymjmutoa' // naturally, replace both with your real credentials or an application-specific password
-//   }
-// });
+  const html = `<h1>${Name}</h1><br/><h1>${Email}</h1><br/><h2>${Numd}</h2><br/><p>${Message}</p>`;
 
-// const mailOptions = {
-//   from: 'romanenglish889@gmail.com',
-//   to: 'romanenglish889@gmail.com',
-//   subject: 'Invoices due',
-//   text: 'Dsvewney.',
-// };
+  const mailOptions = {
+    from:  process.env.EMAIL_ADDRESS,
+    to:  process.env.EMAIL_ADDRESS,
+    subject: `${Name}`,
+    html: html
+  };
 
-// transporter.sendMail(mailOptions, (error, info) =>{
-//   if (error) {
-// 	console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+});
 
-
-app.listen(process.env.PORT || port, () => console.log(`listening on port ${port}!`));
+app.listen(process.env.PORT || port, () => console.log(`listening on port ${port} or ${process.env.PORT}!`));
